2743d6f91f01804b3e96407e2a6e915c216b6624
Marcus Sandes <marcus.sandesf@gmail.com>
2025-08-15 18:27:34 -0300

    commit 'Preparando o ambiente'

--- src/br/com/dio/model/Board.java
+++ src/br/com/dio/model/Board.java
@@ -3,7 +3,8 @@ package br.com.dio.model;
 import java.util.Collection;
 import java.util.List;
 
-import static br.com.dio.model.GameStatusEnum.NON_STARDED;
+import static br.com.dio.model.GameStatusEnum.*;
+import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
 public class Board {
@@ -18,12 +19,52 @@ public class Board {
         return spaces;
     }
 
-    public GameStatusEnum getStatus(){
+    public GameStatusEnum getStatus() {
         if (spaces.stream().flatMap(Collection::stream).noneMatch(s -> !s.isFixed() && nonNull(s.getActual()))) {
             return NON_STARDED;
         }
 
-        return spaces.stream()
+        return spaces.stream().flatMap(Collection::stream).anyMatch(s -> isNull(s.getActual())) ? INCOMPLETE : COMPLETE;
 
     }
+
+    public boolean hasErrors() {
+        if (getStatus() == NON_STARDED) {
+            return false;
+        }
+
+        return spaces.stream().flatMap(Collection::stream)
+                .anyMatch(s -> nonNull(s.getActual()) && !s.getActual().equals(s.getExpected()));
+    }
+
+    public boolean changeValue(final int col, final int row, final int value) {
+        var space = spaces.get(col).get(row);
+        if (space.isFixed()) {
+            return false;
+        }
+
+        space.setActual(value);
+        return true;
+
+    }
+
+    public boolean clearValue(final int col, final int row) {
+        var space = spaces.get(col).get(row);
+        if (space.isFixed()) {
+            return false;
+        }
+
+        space.clearSpace();
+        return true;
+
+    }
+
+    public void reset(){
+        spaces.forEach(c -> c.forEach(Space::clearSpace));
+    }
+
+    public boolean gameIsFinished(){
+        return !hasErrors() && getStatus().equals(COMPLETE);
+    }
+
 }
